a75aad80bd156543d310ae2779f26c6a22a49f8a
linmeideli <2558527373@qq.com>
2026-08-28 11:45:01 +0800

    fix: move google/mavenCentral before aliyun mirrors for CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,12 +9,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven("https://jitpack.io")
-        google()
-        mavenCentral()
     }
 }
 
